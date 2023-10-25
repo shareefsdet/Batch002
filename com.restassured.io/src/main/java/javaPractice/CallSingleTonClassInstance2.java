23f642c28908d3d703c6099d90e9d08ec4773fcf
91900 <91900@ShareefDell>
2023-10-26 00:53:10 +0530

    2nd Commit

---
+++ com.restassured.io/src/main/java/javaPractice/CallSingleTonClassInstance2.java
@@ -0,0 +1,35 @@
+package javaPractice;
+
+class SingleTonPattern {
+
+	private static SingleTonPattern instance = null;
+
+	private SingleTonPattern() {
+
+	}
+
+	public static SingleTonPattern getInstance() {
+
+		if (instance == null) {
+			instance = new SingleTonPattern();
+		}
+
+		return instance;
+
+	}
+
+}
+
+public class CallSingleTonClassInstance2 {
+	public static void main(String[] args) {
+
+		SingleTonPattern obj1 = SingleTonPattern.getInstance();
+		SingleTonPattern obj2 = SingleTonPattern.getInstance();
+		SingleTonPattern obj3 = SingleTonPattern.getInstance();
+
+		System.out.println(obj1 == obj2);
+		System.out.println(obj2 == obj3);
+		System.out.println(obj3 == obj1);
+
+	}
+}
